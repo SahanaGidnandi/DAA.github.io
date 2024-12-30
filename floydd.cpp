d24a7ab1905b74463ad8679def2edfec823d4520
SahanaGidnandi <01fe23bcs185@kletech.ac.in>
2024-12-30 23:57:29 +0530

    Create floydd.cpp

---
+++ floydd.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+#define INF INT_MAX
+#define V 4
+
+void floydWarshall(int graph[V][V]) {
+    int dist[V][V], i, j, k;
+
+    for (i = 0; i < V; i++)
+        for (j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+    for (k = 0; k < V; k++) {
+        for (i = 0; i < V; i++) {
+            for (j = 0; j < V; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF &&
+                    dist[i][j] > dist[i][k] + dist[k][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    cout << "Shortest path matrix:\n";
+    for (i = 0; i < V; i++) {
+        for (j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                cout << "INF ";
+            else
+                cout << dist[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int graph[V][V] = { {0, 5, INF, 10},
+                        {INF, 0, 3, INF},
+                        {INF, INF, 0, 1},
+                        {INF, INF, INF, 0} };
+    
+    floydWarshall(graph);
+    return 0;
+}
